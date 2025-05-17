fe807a632475ff4c6688899862cfde08f3b3584e
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-17 20:40:13 +0300

    добавление conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,71 @@
+import os
+
+import pytest
+from dotenv import load_dotenv
+from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from vc_ru_tests.utils import attach
+
+DEFAULT_BROWSER_VERSION = '128.0'
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default=DEFAULT_BROWSER_VERSION,
+        help='browser version (chrome: 128.0, 127.0)'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='session', autouse=True)
+def browser_manager(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != '' else DEFAULT_BROWSER_VERSION
+
+    options = Options()
+    selenoid_capabilities = {
+        'browserName': 'chrome',
+        'browserVersion': browser_version,
+        'selenoid:options': {
+            'enableVNC': True,
+            'enableVideo': True,
+            'screenResolution': '1920x1080x24'
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    selenoid_login = os.getenv('SELENOID_LOGIN')
+    selenoid_pass = os.getenv('SELENOID_PASS')
+    selenoid_url = os.getenv('SELENOID_URL')
+
+    driver = webdriver.Remote(
+        command_executor=f'https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub',
+        options=options
+    )
+
+    browser.config.driver = driver
+    browser.config.base_url = ('https://vc.ru')
+    browser.driver.maximize_window()
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_open(browser_manager):
+    browser.open('/')
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
