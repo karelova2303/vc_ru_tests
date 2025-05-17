cc8b4ed6838d71862e648ab47cf461c9688be892
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-17 20:40:23 +0300

    добавление data.py

---
+++ vc_ru_tests/data/data.py
@@ -0,0 +1,55 @@
+subsite_card_info = {'Сервисы': 'services',
+                     'Маркетинг': 'marketing',
+                     'Личный опыт': 'life',
+                     'Деньги': 'money',
+                     'AI': 'ai',
+                     'Телеграм': 'telegram',
+                     'Право': 'legal',
+                     'Образование': 'education',
+                     'Инвестиции': 'invest',
+                     'Соцсети': 'social',
+                     'Крипто': 'crypto',
+                     'Карьера': 'hr',
+                     'Дизайн': 'design',
+                     'Путешествия': 'travel',
+                     'Мнения': 'opinions',
+                     'Техника': 'tech',
+                     'Маркетплейсы': 'marketplace',
+                     'Разработка': 'dev',
+                     'Приёмная': 'claim',
+                     'Кейсы роста': 'growth',
+                     'Транспорт': 'transport',
+                     'Что почитать': 'books',
+                     'Еда': 'food',
+                     'SEO': 'seo',
+                     'Будущее': 'future',
+                     'Миграция': 'migration',
+                     'Истории': 'story',
+                     'Офтоп': 'flood',
+                     'Трибуна': 'tribuna',
+                     'Ритейл': 'retail',
+                     'Находки': 'links',
+                     'Видео': 'video',
+                     'Офлайн': 'offline',
+                     'Медиа': 'media',
+                     'ChatGPT': 'chatgpt',
+                     'Вопросы': 'ask',
+                     'Apple': 'apple',
+                     'Офис': 'office',
+                     'Будни': 'workdays',
+                     'Midjourney': 'midjourney',
+                     'Конкурсы': 'contest'
+                     }
+
+author_name_vc = 'vc.ru'
+about_project_title = 'проекте'
+
+rules = 'Правила'
+rules_title = 'Правила общения на vc.ru'
+
+advertising = 'Реклама'
+author_name_promo = 'Промо'
+advertising_name = 'Какую рекламу можно запустить на vc.ru'
+
+apps = 'Приложения'
+apps_title = 'Этот QR-код поможет скачать наше приложение'
