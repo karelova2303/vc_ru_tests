d0e604cc066defe9d7844ad03118e3a13b8046d3
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-20 00:31:07 +0300

    обновлен app.py

--- vc_ru_tests/models/app.py
+++ vc_ru_tests/models/app.py
@@ -1,10 +1,10 @@
 from vc_ru_tests.models.pages.main_page import MainPage
-from vc_ru_tests.models.pages.sidebar_page import SideBarPage
+from vc_ru_tests.models.pages.sidebar_components_page import SideBarComponentsPage
 
 
 class Application:
     def __init__(self):
         self.main_page = MainPage()
-        self.sidebar_page = SideBarPage()
+        self.sidebar_components = SideBarComponentsPage()
 
 app = Application()
