418fdc4f9361ed7f3443854619a4626218a37a7e
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-20 00:54:53 +0300

    обновлен conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -53,7 +53,7 @@ def browser_manager(request):
     yield browser
 
     attach.add_screenshot(browser)
-    attach.add_logs(browser)
+    # attach.add_logs(browser)
     attach.add_html(browser)
     attach.add_video(browser)
 
