d999476b8f9347a924071ad9475ffebe606d7b53
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-20 00:32:03 +0300

    обновлен test_vc_ru.py

--- tests/test_vc_ru.py
+++ tests/test_vc_ru.py
@@ -10,8 +10,14 @@ from vc_ru_tests.models.app import app
 @allure.suite('Хедер')
 @allure.title('Проверка отображения компонентов хедера при открытии страницы')
 @allure.link('https://vc.ru')
-def test_header(browser_open):
-    app.main_page.should_be_visible_header()
+def test_header():
+    app.main_page.open_main_page()
+
+    app.main_page.should_be_visible_logo_in_header()
+    app.main_page.should_be_visible_main_header()
+    app.main_page.should_be_visible_search_button()
+    app.main_page.should_be_visible_button_editor()
+    app.main_page.should_be_visible_button_auth()
 
 
 @allure.tag('Web', 'Prod')
@@ -19,11 +25,11 @@ def test_header(browser_open):
 @allure.suite('Боковая панель "Темы"')
 @allure.title('Проверка контента карточек на соответствие выбранной темы из боковой панели')
 @allure.link('https://vc.ru')
-def test_sidebar_topics(browser_open):
-    with allure.step('Кликаем кнопку "Показать все"'):
-        app.sidebar_page.clickable_icon_chevron_down()
+def test_sidebar_topics():
+    app.main_page.open_main_page()
 
-    app.sidebar_page.should_be_visible_content_sidebar_section_topics(subsite_card_info)
+    app.sidebar_components.clickable_icon_chevron_down()
+    app.sidebar_components.should_be_visible_content_sidebar_section_topics(subsite_card_info)
 
 
 @allure.tag('Web', 'Prod')
@@ -31,12 +37,14 @@ def test_sidebar_topics(browser_open):
 @allure.suite('Боковая панель "vc.ru"')
 @allure.title('Проверка контента карточек на соответствие выбранному разделу боковой панели')
 @allure.link('https://vc.ru')
-def test_sidebar_info(browser_open):
-    with allure.step(f'Выбираем раздел "О {about_project_title}"'):
-        app.sidebar_page.should_be_visible_content_about_project(author_name_vc, about_project_title)
-    with allure.step(f'Выбираем раздел "{rules}"'):
-        app.sidebar_page.should_be_visible_content_rules(author_name_vc, rules_title)
-    with allure.step(f'Выбираем раздел "{advertising}"'):
-        app.sidebar_page.should_be_visible_content_advertising(author_name_promo, advertising_name)
-    with allure.step(f'Выбираем раздел "{apps}"'):
-        app.sidebar_page.should_be_visible_content_apps(apps_title)
+def test_sidebar_info():
+    app.main_page.open_main_page()
+
+    with allure.step(f'Проверяем раздел "О {about_project_title}"'):
+        app.sidebar_components.should_be_visible_content_about_project(author_name_vc, about_project_title)
+    with allure.step(f'Проверяем раздел "{rules}"'):
+        app.sidebar_components.should_be_visible_content_rules(author_name_vc, rules_title)
+    with allure.step(f'Проверяем раздел "{advertising}"'):
+        app.sidebar_components.should_be_visible_content_advertising(author_name_promo, advertising_name)
+    with allure.step(f'Проверяем раздел "{apps}"'):
+        app.sidebar_components.should_be_visible_content_apps(apps_title)
