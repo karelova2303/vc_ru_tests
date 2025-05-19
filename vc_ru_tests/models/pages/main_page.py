8424b2b21e6983e9f79c8c5854c7de80a32cd462
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-20 00:31:37 +0300

    обновлен main_page.py

--- vc_ru_tests/models/pages/main_page.py
+++ vc_ru_tests/models/pages/main_page.py
@@ -1,17 +1,35 @@
 import allure
-from selene import browser, be
+from selene import browser, be, have
 
 
 class MainPage:
     def __init__(self):
-        self.header_left = browser.element('.header__left')
+        self.header_left = browser.element('.header__left > a')
         self.header_main = browser.element('.header__main')
-        self.header_right = browser.element('.header__right')
+        self.search_button = browser.element('.search__button')
+        self.button_editor = browser.element('.header__right > [href="/editor"] > button')
+        self.button_auth = browser.element('.header__right > [href="/auth"] > button')
 
-    def should_be_visible_header(self):
-        with allure.step('Компоненты в левой части хедера отображаются'):
-            self.header_left.should(be.visible)
-        with allure.step('Компоненты в центре хедера отображаются'):
-            self.header_main.should(be.visible)
-        with allure.step('Компоненты в правой части хедера отображаются'):
-            self.header_right.should(be.visible)
+    @allure.step('Открываем Главную страницу')
+    def open_main_page(self):
+        browser.open('/')
+
+    @allure.step('Проверяем отображение логотипа')
+    def should_be_visible_logo_in_header(self):
+        self.header_left.should(be.visible)
+
+    @allure.step('Проверяем отображение основного контента хедера')
+    def should_be_visible_main_header(self):
+        self.header_main.should(be.visible)
+
+    @allure.step('Проверяем отображение кнопки поиска')
+    def should_be_visible_search_button(self):
+        self.search_button.should(be.visible)
+
+    @allure.step('Проверяем отображение кнопки "Написать"')
+    def should_be_visible_button_editor(self):
+        self.button_editor.should(be.visible)
+
+    @allure.step('Проверяем отображение кнопки "Войти"')
+    def should_be_visible_button_auth(self):
+        self.button_auth.should(be.visible)
