3b8bc64f39b32099f3c1b34f6f2dff99dc4d5f7e
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-20 22:49:31 +0300

    обновлен conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,38 +8,37 @@ from selenium.webdriver.chrome.options import Options
 
 from vc_ru_tests.utils import attach
 
-DEFAULT_BROWSER_VERSION = '128.0'
 
-
-def pytest_addoption(parser):
-    parser.addoption('--browser_version', default=DEFAULT_BROWSER_VERSION)
-
-
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def load_env():
     load_dotenv()
 
 
-@pytest.fixture(scope='session', autouse=True)
-def browser_manager(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != '' else DEFAULT_BROWSER_VERSION
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='128.0'
+    )
 
+
+@pytest.fixture(scope='session')
+def browser_manager(request):
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
+    browser_version = request.config.getoption('--browser_version')
     options = Options()
+
     selenoid_capabilities = {
-        'browserName': 'chrome',
-        'browserVersion': browser_version,
-        'selenoid:options': {
-            'enableVNC': True,
-            'enableVideo': True,
-            'screenResolution': '1920x1080x24'
+        "browserName": 'chrome',
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
-
-    selenoid_login = os.getenv('SELENOID_LOGIN')
-    selenoid_pass = os.getenv('SELENOID_PASS')
-    selenoid_url = os.getenv('SELENOID_URL')
 
     driver = webdriver.Remote(
         command_executor=f'https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub',
@@ -47,13 +46,15 @@ def browser_manager(request):
     )
 
     browser.config.driver = driver
-    browser.config.base_url = ('https://vc.ru')
-    browser.driver.maximize_window()
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
+    browser.config.base_url = 'https://vc.ru'
+
 
     yield browser
 
     attach.add_screenshot(browser)
-    # attach.add_logs(browser)
+    attach.add_logs(browser)
     attach.add_html(browser)
     attach.add_video(browser)
 
